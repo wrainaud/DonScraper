1eb81416871f1b7a88811f5b61dd0f666f466e89
William Rainaud <wrainaud@gmail.com>
2017-12-08 23:44:32 -0500

    running low on time

---
+++ controllers/article.js
@@ -0,0 +1,20 @@
+
+var cheerio;
+var request;
+
+exports.getArticles = function(req, res, next) {
+  cheerio = require('cheerio');
+  request = require('request');
+
+  request.get('https://news.ycombinator.com/', function(err, request, body) {
+    var $ = cheerio.load(body);
+    var links = [];
+    $('.title a[href^="http"], a[href^="https"]').each(function() {
+      links.push($(this));
+    });
+    res.render('/articles', {
+      title: 'Articles',
+      links: links
+    });
+  });
+};
